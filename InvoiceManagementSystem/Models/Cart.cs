09357e3438c9486b058f1774bd74554a1bef3ef2
manish-eg <makul@eg.dk>
2024-06-28 16:05:47 +0530

    cart model is created

---
+++ InvoiceManagementSystem/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿namespace InvoiceManagementSystem.Models
+{
+    public class Cart:BaseEntity
+    {
+        public readonly string CartID;
+        public string CustomerID { get; set; }
+        public string ProductID { get; set; }
+
+        public Cart(string customerID,string productID) {
+            CartID = "CART" + CartId;
+            CustomerID = customerID;
+            ProductID = productID;
+        }
+    }
+}
