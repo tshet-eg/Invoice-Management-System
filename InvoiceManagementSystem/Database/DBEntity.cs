df0b0a046dba85a41b7a132d1f546607f5caebcf
manish-eg <makul@eg.dk>
2024-06-28 12:51:20 +0530

    all the list are made as static

--- InvoiceManagementSystem/Database/DBEntity.cs
+++ InvoiceManagementSystem/Database/DBEntity.cs
@@ -4,9 +4,9 @@ namespace InvoiceManagementSystem.Database
 {
     public class DBEntity
     {
-        public List<Customer>CustomerList=new List<Customer>();
-        public List<Category>CategoryList=new List<Category>();
-        public List<Product>ProductList=new List<Product>();
-        public List<Cart>Cart=new List<Cart>();
+        public static List<Customer>CustomerList=new List<Customer>();
+        public static List<Category>CategoryList=new List<Category>();
+        public static List<Product>ProductList=new List<Product>();
+        public static List<Cart>Cart=new List<Cart>();
     }
 }
