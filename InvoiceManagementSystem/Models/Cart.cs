62a81945d55331145073e2387763373e2729f2b8
manish-eg <makul@eg.dk>
2024-06-28 16:10:23 +0530

    quantity field added to cart

--- InvoiceManagementSystem/Models/Cart.cs
+++ InvoiceManagementSystem/Models/Cart.cs
@@ -5,11 +5,13 @@
         public readonly string CartID;
         public string CustomerID { get; set; }
         public string ProductID { get; set; }
+        public int Quantity { get;set }
 
-        public Cart(string customerID,string productID) {
+        public Cart(string customerID,string productID,int quantity) {
             CartID = "CART" + CartId;
             CustomerID = customerID;
             ProductID = productID;
+            Quantity = quantity;
         }
     }
 }
