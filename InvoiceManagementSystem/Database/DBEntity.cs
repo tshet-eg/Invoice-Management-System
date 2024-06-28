bd70328dddd154b695d83447430e78fbb2623eb3
manish-eg <makul@eg.dk>
2024-06-28 10:20:17 +0530

    DBEntity class created

---
+++ InvoiceManagementSystem/Database/DBEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace InvoiceManagementSystem.Database
+{
+    public class DBEntity
+    {
+        public List<Customer>=new List<Customer>();
+        public List<Category>=new List<Category>();
+        public List<Product>=new List<Product>();
+        public List<Cart>=new List<Cart>();
+    }
+}
