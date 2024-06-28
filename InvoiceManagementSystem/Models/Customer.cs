d2f79253db98364b5c67b3c0915e4e6dc8737588
tshet-eg <tshet@eg.dk>
2024-06-28 14:32:48 +0530

    created model class for customer properties and fields

--- InvoiceManagementSystem/Models/Customer.cs
+++ InvoiceManagementSystem/Models/Customer.cs
@@ -1,7 +1,22 @@
-﻿
-namespace InvoiceManagementSystem.Models
+﻿namespace InvoiceManagementSystem.Models
 {
-    internal class CustomerModel
+    public class Customer : BaseEntity
     {
+        private int _customerID;
+        public int CustomerID
+        {
+            get
+            {
+                return _customerID;
+            }
+            set
+            {
+                _customerID = CustomerId;
+            }
+        }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public long PhoneNumber {  get; set; }
+        public string Address { get; set; }
     }
 }
