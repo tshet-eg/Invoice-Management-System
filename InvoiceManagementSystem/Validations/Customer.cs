d7095dc4b0fbe245550d20abca58cfc65229754f
manish-eg <makul@eg.dk>
2024-06-28 12:54:32 +0530

    validation methods are made as static

--- InvoiceManagementSystem/Validations/Customer.cs
+++ InvoiceManagementSystem/Validations/Customer.cs
@@ -4,10 +4,10 @@ namespace InvoiceManagementSystem.Validations
 {
     public class Customer
     {
-        public bool ValidateEmail(string email) {
+        public static bool ValidateEmail(string email) {
             return Regex.IsMatch(email, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
         }
-        public bool ValidatePhone(long phoneNumber) {
+        public static bool ValidatePhone(long phoneNumber) {
             return Regex.IsMatch(phoneNumber.ToString(), @"^(?!0)(?!.*(\d)\1{9})\d{10}$");
         }
     }
