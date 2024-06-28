df1f307635b84f11381fa07968bba60c02c20931
manish-eg <makul@eg.dk>
2024-06-28 10:35:40 +0530

    DBEntity class is created

--- InvoiceManagementSystem/Database/DBEntity.cs
+++ InvoiceManagementSystem/Database/DBEntity.cs
@@ -4,9 +4,9 @@ namespace InvoiceManagementSystem.Database
 {
     public class DBEntity
     {
-        public List<Customer>=new List<Customer>();
-        public List<Category>=new List<Category>();
-        public List<Product>=new List<Product>();
-        public List<Cart>=new List<Cart>();
+        public List<Customer>CustomerList=new List<Customer>();
+        public List<Category>CategoryList=new List<Category>();
+        public List<Product>ProductList=new List<Product>();
+        public List<Cart>Cart=new List<Cart>();
     }
 }
